a6c30ba820cb2598ff99921ae0b39e96f6321112
Ben Lower <github@benlower.us>
2015-08-12 16:15:43 -0700

    added columns and getting data

--- javascripts/foodInspectionWDC.js
+++ javascripts/foodInspectionWDC.js
@@ -1,59 +1,5 @@
-
-function requestMSHProfileByUrl(url) {
-	console.log('about to get our data...');
-	
-	$.getJSON(url, function(json, status, xhr) {
-		// Lets start making some rows
-		//var rows = processResponse(json);				// todo for more complex data
-		
-		console.log('our json: ' + JSON.stringify(json));
-		
-		// We have to have default values or else any missing data in our return json will screw us up
-		// TODO -> make sure our datetimes are formatted as Tableau expects
-		var rows = {
-			'firstName': json.firstName || "",
-			'middleName': json.middleName || "",
-			'lastName': json.lastName || "",
-			'lastUpdateTime': MSHDateToTableauDate(json.lastUpdateTime) || "",
-			'birthdate': MSHDateToTableauDate(json.birthdate) || "",
-			'postalCode': json.postalCode || "",
-			'gender': json.gender || "",
-			'height': json.height || "",
-			'weight': json.weight || ""
-		};
-
-		var stringRep = JSON.stringify(rows);
-		console.log('stringRep: ' + stringRep);
-
-		var pagingLink = xhr.getResponseHeader("Link");
-		var token = "";
-		if (pagingLink) {
-			stringRep = pagingLink.toString();
-			if (stringRep.length > 0) {
-				// we get something like <https://connect.squareup.com/v1/me/payments?limit=2&batch_token=QXZxWjgybEx…in_time=2014-07-31T23%3A52%3A35.489Z&end_time=2015-07-31T23%3A52%3A35.489Z>;rel='next'
-				// back from the api. parse our just the link (I'm sure there's a better way)
-				token = stringRep.substring(1, stringRep.indexOf(">;rel='next'"));
-			}
-		}
-
-		console.log('about to tableau.dataCallback: ' + JSON.stringify(rows));
-  		tableau.dataCallback(rows, token, token.length > 0);
-	}); 
-}
-
-function requestMSHProfile(accessToken) {
-	console.log('in requestMSHProfile');
-    $.ajaxSetup({
-  		headers : {'Authorization': 'bearer ' + accessToken}
-	});
-
-    var url = 'https://api.microsofthealth.net/v1/me/Activities/';
-
-    requestMSHProfileByUrl(url);
-}
-
 // Use Moment to convert dates to acceptible format for Tableau
-function MSHDateToTableauDate(dateToConvert) {
+function dateToTableauDate(dateToConvert) {
 	// Use moment
 	var moDate = moment(dateToConvert).format("YYYY-MM-DD HH:mm:ss.SSS");
 	
@@ -61,25 +7,18 @@ function MSHDateToTableauDate(dateToConvert) {
 }
 
 $(document).ready(function() {
-	var accessToken = Cookies.get("accessToken");
-	
 	$("#getDataButton").click(function() {
-		console.log('getDataButton clicked!');
-		tableau.connectionName = "MS Health Data";
+		var zipCode = $('#zipCode').val().trim();
+		console.log("zipCode: " + zipCode);
+		
+		// If we have a Zipcode then set it as tableau connection data so we can use it later
+		if(zipCode) {
+			tableau.connectionData = zipCode;
+			tableau.connectionName = "King County Inspection Data: " + zipCode;
+		}
+		
 		tableau.submit();
 	});
-
-	var hasAuth = accessToken && accessToken.length > 0;
-
-	if (hasAuth) {
-		$("#notsignedin").css('display', 'none');
-		$("#signedin").css('display', 'inline');
-		$("#getDataButton").prop("disabled",false);
-	} else {
-		$("#notsignedin").css('display', 'inline');
-		$("#signedin").css('display', 'none');
-		$("#getDataButton").prop("disabled",true);
-	}
 });
 
 // -------------------------------------------------- //
@@ -88,57 +27,66 @@ $(document).ready(function() {
 var myConnector = tableau.makeConnector();
 
 myConnector.init = function() {
-	console.log('init');
-	var accessToken = Cookies.get("accessToken");
-
-	var hasAuth = (accessToken && accessToken.length > 0) || tableau.password.length > 0;
-
-	if (hasAuth) {
-		$("#notsignedin").css('display', 'none');
-		$("#signedin").css('display', 'inline');
-		$("#getDataButton").prop("disabled",false);
-	} else {
-		$("#notsignedin").css('display', 'inline');
-		$("#signedin").css('display', 'none');
-		$("#getDataButton").prop("disabled",true);
-	}
-
-	if (tableau.phase == tableau.phaseEnum.interactivePhase || tableau.phase == tableau.phaseEnum.authPhase) {
-		if (hasAuth) {
-			tableau.initCallback();
-			tableau.password = accessToken;
-
-			return;
-		}
-	} else {
-		if (!hasAuth) {
-			tableau.abortWithError("Don't have an access token. Giving up");
-		}
-	}
-
 	tableau.initCallback();
 };
 
 
 myConnector.getColumnHeaders = function() {
-	console.log('getColumHeaders');
-	
-	var fieldNames = ['firstName', 'middleName', 'lastName', 'lastUpdateTime', 'birthdate', 'postalCode', 'gender', 'height', 'weight'];
-    var fieldTypes = ['string', 'string', 'string', 'datetime', 'datetime', 'string', 'string', 'int', 'int'];
+	var fieldNames = ['inspection_result', 'inspection_business_name', 'zip_code', 'inspection_score', 'inspection_type', 'violation_description', 'violation_record_id', 'inspection_closed_business', 'city', 'violation_type', 'inspection_date', 'inspection_serial_num', 'address', 'description', 'name', 'business_id', 'longitude', 'latitude', 'program_identifier', 'violation_points'];
+    
+	var fieldTypes = ['string', 'string', 'string', 'int', 'string', 'string', 'string', 'bool', 'string', 'string', 'datetime', 'string', 'string', 'string', 'string', 'string', 'float', 'float', 'string', 'int'];
 
   tableau.headersCallback(fieldNames, fieldTypes);
 };
 
-
-myConnector.getTableData = function(lastRecordToken) {
-	console.log('in getTableData');
+myConnector.getTableData = function() {
+	var zipCode = tableau.connectionData;
+	var url = 'https://data.kingcounty.gov/resource/f29f-zza5.json?zip_code=' + zipCode;
 	
-	if (lastRecordToken && lastRecordToken.length > 0) {
-		requestMSHProfileByUrl(lastRecordToken);
-	} else {
-	    var accessToken = tableau.password;
-	    requestMSHProfile(accessToken);	
-	}
+	$.getJSON(url, function(json, status, xhr) {
+		if(json) {
+			console.log('our json: ' + JSON.stringify(json));
+			
+			var i;
+			var toRet = [];
+			
+			for(i = 0; i < json.length; i++) {
+				var row = {
+					"inspection_result" : json[i]["inspection_result"] || null,
+					"inspection_business_name" : json[i]["inspection_business_name"] || null,
+					"zip_code" : json[i]["zip_code"] || null,
+					"inspection_score" : json[i]["inspection_score"] || null,
+					"inspection_type" : json[i]["inspection_type"] || null,
+					"violation_description" : json[i]["violation_description"] || null,
+					"violation_record_id" : json[i]["violation_record_id"] || null,
+					"inspection_closed_business" : json[i]["inspection_closed_business"] || null,
+					"city" : json[i]["city"] || null,
+					"violation_type" : json[i]["violation_type"] || null,
+					"inspection_date" : dateToTableauDate(json[i]["inspection_date"]) || null,			// use a date format tableau knows
+					"inspection_serial_num" : json[i]["inspection_serial_num"] || null,
+					"address" : json[i]["address"] || null,
+					"description" : json[i]["description"] || null,
+					"name" : json[i]["name"] || null,
+					"business_id" : json[i]["business_id"] || null,
+					"longitude" : json[i]["longitude"] || null,
+					"latitude" : json[i]["latitude"] || null,
+					"program_identifier" : json[i]["program_identifier"] || null,
+					"violation_points" : json[i]["violation_points"] || null,
+										
+				};
+				
+				toRet.push(row);
+			}
+			
+			// call back to tableau with data
+			tableau.dataCallback(toRet, toRet.length.toString(), false);
+			
+		} else {
+			console.log('error: ' + xhr.responseText);
+			tableau.abortWithError("error getting data");
+		}
+	}); 
+
 };
 
 
