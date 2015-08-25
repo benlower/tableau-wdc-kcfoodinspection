a7d58b783b0c0e2bb86814646df083550bd7cdf7
Ben Lower <github@benlower.us>
2015-08-24 18:19:39 -0700

    fixing up the for loop
    also added in missing moment library

--- javascripts/foodInspectionWDC.js
+++ javascripts/foodInspectionWDC.js
@@ -26,8 +26,43 @@
         
         $.getJSON(
           url,
-          function(data, status) {
-            tableau.log("Request received: " + JSON.stringify(data));
+          function (data, status) {
+            tableau.log("Data received!");
+
+            if (data) {
+              var i;
+              var toRet = [];
+
+              for (i = 0; i < data.length; i++) {
+                var row = {
+                  "inspection_result": data[i]["inspection_result"] || null,
+                  "inspection_business_name": data[i]["inspection_business_name"] || null,
+                  "zip_code": data[i]["zip_code"] || null,
+                  "inspection_score": data[i]["inspection_score"] || null,
+                  "inspection_type": data[i]["inspection_type"] || null,
+                  "violation_description": data[i]["violation_description"] || null,
+                  "violation_record_id": data[i]["violation_record_id"] || null,
+                  "inspection_closed_business": data[i]["inspection_closed_business"] || null,
+                  "city": data[i]["city"] || null,
+                  "violation_type": data[i]["violation_type"] || null,
+                  "inspection_date": dateToTableauDate(data[i]["inspection_date"]) || null,			// use a date format tableau knows
+                  "inspection_serial_num": data[i]["inspection_serial_num"] || null,
+                  "address": data[i]["address"] || null,
+                  "description": data[i]["description"] || null,
+                  "name": data[i]["name"] || null,
+                  "business_id": data[i]["business_id"] || null,
+                  "longitude": data[i]["longitude"] || null,
+                  "latitude": data[i]["latitude"] || null,
+                  "program_identifier": data[i]["program_identifier"] || null,
+                  "violation_points": data[i]["violation_points"] || null
+                };
+
+                toRet.push(row);
+              
+                // Call back to tableau with the table data and the new record number (this is stored as a string)
+                tableau.dataCallback(toRet, toRet.length.toString(), false);
+              }
+            }
           }
           );
         // $.ajax({
@@ -37,38 +72,7 @@
         //     if (data) {
         //       console.log('data: ' + data);
         //     
-        //       // 				var i;
-        //       // 				var toRet = [];
-        //       // 				
-        //       // 				for(i = 0; i < data.length; i++) {
-        //       // 					var row = {
-        //       // 						"inspection_result" : json[i]["inspection_result"] || null,
-        //       // 						"inspection_business_name" : json[i]["inspection_business_name"] || null,
-        //       // 						"zip_code" : json[i]["zip_code"] || null,
-        //       // 						"inspection_score" : json[i]["inspection_score"] || null,
-        //       // 						"inspection_type" : json[i]["inspection_type"] || null,
-        //       // 						"violation_description" : json[i]["violation_description"] || null,
-        //       // 						"violation_record_id" : json[i]["violation_record_id"] || null,
-        //       // 						"inspection_closed_business" : json[i]["inspection_closed_business"] || null,
-        //       // 						"city" : json[i]["city"] || null,
-        //       // 						"violation_type" : json[i]["violation_type"] || null,
-        //       // 						"inspection_date" : dateToTableauDate(json[i]["inspection_date"]) || null,			// use a date format tableau knows
-        //       // 						"inspection_serial_num" : json[i]["inspection_serial_num"] || null,
-        //       // 						"address" : json[i]["address"] || null,
-        //       // 						"description" : json[i]["description"] || null,
-        //       // 						"name" : json[i]["name"] || null,
-        //       // 						"business_id" : json[i]["business_id"] || null,
-        //       // 						"longitude" : json[i]["longitude"] || null,
-        //       // 						"latitude" : json[i]["latitude"] || null,
-        //       // 						"program_identifier" : json[i]["program_identifier"] || null,
-        //       // 						"violation_points" : json[i]["violation_points"] || null,
-        //       // 											
-        //       // 					};
-        //       // 					
-        //       // 					toRet.push(row);
-        //       // 
-        //       // 			// Call back to tableau with the table data and the new record number (this is stored as a string)
-        //       // 			tableau.dataCallback(toRet, toRet.length.toString(), false);
+
         //     } else {
         //       tableau.abortWithError("No results found for ticker symbol: " + ticker);
         //     }
