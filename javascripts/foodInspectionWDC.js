49285cb96bbfba98f6a27d0dea1b30d0e17d3a56
Ben Lower <github@benlower.us>
2015-08-25 08:29:00 -0700

    cleaning up comments

--- javascripts/foodInspectionWDC.js
+++ javascripts/foodInspectionWDC.js
@@ -8,7 +8,7 @@
         tableau.initCallback();
       };
     
-    
+      // Set-up our column headers so Tableau knows what to expect
       myConnector.getColumnHeaders = function () {
 		  tableau.log('getColumnHeaders...');
         var fieldNames = ['inspection_result', 'inspection_business_name', 'zip_code', 'inspection_score', 'inspection_type', 'violation_description', 'violation_record_id', 'inspection_closed_business', 'city', 'violation_type', 'inspection_date', 'inspection_serial_num', 'address', 'description', 'name', 'business_id', 'longitude', 'latitude', 'program_identifier', 'violation_points'];
@@ -17,19 +17,19 @@
     
         tableau.headersCallback(fieldNames, fieldTypes);
       };
-    
+
+      // Magic time -> call the API to get data, then flatten it in a loop and return to Tableau
+      // More information about the King County inspection data can be found at http://kingcounty.gov/healthservices/health/ehs/foodsafety/inspections/system.aspx
       myConnector.getTableData = function () {
         var zipCode = tableau.connectionData;
         var url = 'https://data.kingcounty.gov/resource/f29f-zza5.json?zip_code=' + zipCode;
-    
-        tableau.log('about to call ' + url);
         
         $.getJSON(
           url,
           function (data, status) {
-            tableau.log("Data received!");
-
-            if (data) {
+            // We got data -> do the work here
+            if (data && data.length > 0) {
+              tableau.log("We got data back from the API call!");
               var i;
               var toRet = [];
 
@@ -64,55 +64,39 @@
               // Call back to tableau with the table data and the new record number (this is stored as a string)
               tableau.dataCallback(toRet, toRet.length.toString(), false);
             }
+            // No data
+            else {
+              tableau.log("Sad panda.  No data.");
+            }
           }
           );
-        // $.ajax({
-        //   url: url,
-        //   dataType: 'text',
-        //   success: function (data) {
-        //     if (data) {
-        //       console.log('data: ' + data);
-        //     
-
-        //     } else {
-        //       tableau.abortWithError("No results found for ticker symbol: " + ticker);
-        //     }
-        //   },
-        //   error: function (xhr, ajaxOptions, thrownError) {
-        //     // add something to the log and return an empty set if there was problem with the connection
-        //     tableau.log("connection error: " + xhr.responseText + "\n" + thrownError);
-        //     tableau.abortWithError("error connecting to the data source");
-        //   }
-        // });
-    
       };
-    
+      
+      // This is used to put our zip code into connection data
       myConnector.setZipCode = function (zipCode) {
-        tableau.log("setting zip code to " + zipCode);
         tableau.connectionData = zipCode;
         tableau.connectionName = "King County Inspection Data: " + zipCode;
       };
     
       tableau.registerConnector(myConnector);
       
+      
       // Use Moment to convert dates to acceptible format for Tableau
+      // Tableau's accepted date formats are at http://onlinehelp.tableau.com/current/api/wdc/en-us/help.htm?#WDC/wdc_ref_date_formats.htm
       function dateToTableauDate(dateToConvert) {
-        // Use moment
-        var moDate = moment(dateToConvert).format("YYYY-MM-DD HH:mm:ss.SSS");
-    
-        return moDate;
+        var moDate = moment(dateToConvert).format("YYYY-MM-DD HH:mm:ss.SSS");       // Moment takes just about anything resembling a date
+        return moDate;                                                              // return our nicely formatted datetime
       }
-    
+      
+      // Our page is ready -> set-up our button click handler which will grab our zip code and work magic
       $(document).ready(function () {
         $("#getDataButton").click(function () {
-          console.log('button click');
           var zipCode = $('#zipCode').val().trim();
-          tableau.log('zip code ' + zipCode);
           
           // If we have a Zipcode then set it as tableau connection data so we can use it later
           if (zipCode) {
+            tableau.log('Our zip code: ' + zipCode + ' Calling tableau.submit()');
             myConnector.setZipCode(zipCode);
-            tableau.log('before calling submit().  tableau.phase: ' + tableau.phase);
             tableau.submit();
           }
         });
